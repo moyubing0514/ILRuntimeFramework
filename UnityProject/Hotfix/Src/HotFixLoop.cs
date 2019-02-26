a9e4d19e343353a59518354428034a6d49fafbfc
moyubing0514 <446541451@qq.com>
2019-02-26 21:17:10 +0800

    msc.rsp

---
+++ UnityProject/Hotfix/Src/HotFixLoop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HotFix
+{
+    public class HotFixLoop : IGameHotFixInterface
+    {
+        private static HotFixLoop m_Instance;
+        private Mono2DllFunction m_Mono2DllFunction = new Mono2DllFunction();
+
+
+        public override void Start()
+        {
+            m_Instance = this;
+            Debug.Log("HotFix Start Ok");
+        }
+
+        public override void Update()
+        {
+
+        }
+
+        private void OnCameraPositionChangedEvent()
+        {
+        }
+
+        public static HotFixLoop GetInstance()
+        {
+            return m_Instance;
+        }
+
+        public override void OnDestroy()
+        {
+
+        }
+        public override void OnApplicationQuit()
+        {
+            UnityEngine.Debug.Log("HotFixLoop OnApplicationQuit");
+
+        }
+
+        public override object OnMono2GameDll(string func, params object[] data)
+        {
+            return m_Mono2DllFunction.OnMono2GameDll(func, data);
+        }
+    }
+}
---
+++ UnityProject/Hotfix/Src/Mono2DllFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotFix
+{
+    public class Mono2DllFunction
+    {
+        public object OnMono2GameDll(string func, params object[] data)
+        {
+            if (func == "GameResultMessageHF_OnGameOver")
+            {
+                //GameResultMessageHF_OnGameOver(data);
+            }
+            else if(func == "PlayerMyself_GetId")
+            {
+                return 0;// PlayerMyself_GetId();
+            }
+            return null;
+        }
+    }
+}
